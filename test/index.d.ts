d865466b19159a2140da14327ba7969f1e4d9753
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2019-03-13 16:40:37 +0100

    Simplify type

--- test/index.d.ts
+++ test/index.d.ts
@@ -1,5 +1,5 @@
 declare module 'es-observable-tests' {
-  export function runTests(klazz: {
-    new (...args: any[]): any
-  }): Promise<{ logger: { passed: number; errored: number; failed: number } }>
+  export function runTests(
+    klazz: new (...args: any[]) => any
+  ): Promise<{ logger: { passed: number; errored: number; failed: number } }>
 }
